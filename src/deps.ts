9f3f8dfa825eb5bb974dff2d37d52153193b4d05
oganexon <oganexon@protonmail.com>
2021-06-04 12:03:21 +0200

    WIP: cla action

---
+++ src/deps.ts
@@ -0,0 +1,6 @@
+/* std */
+export { parse as parseFlags } from "https://deno.land/std@0.97.0/flags/mod.ts";
+export { format as formatTime } from "https://deno.land/std@0.97.0/datetime/mod.ts";
+
+/* octokit */
+export { Octokit } from "https://cdn.skypack.dev/@octokit/core@^3.4.0";
