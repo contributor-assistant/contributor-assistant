c9cc8ef177fbfa756c12a238bab64420267add70
oganexon <oganexon@protonmail.com>
2021-07-12 17:10:29 +0200

    fix: css

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,7 +1,6 @@
 import App from './App.svelte'
 
 import "bulma/css/bulma.css";
-import "https://kit.fontawesome.com/5210e48b75.js"
 
 const app = new App({
   target: document.getElementById('app')
